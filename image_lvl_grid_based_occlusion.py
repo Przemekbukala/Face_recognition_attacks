453c1accf9e665098abfea88f71287d1164fca24
Kuba <jakub.led2017@gmail.com>
2026-05-13 08:21:48 +0000

    Added: setup for env and face recognition model

---
+++ image_lvl_grid_based_occlusion.py
@@ -0,0 +1,11 @@
+# Nakłada na obraz siatkę czarnych linii (1 px).
+# ⚙️ Mechanizm
+# 	wybierasz punkty na krawędziach obrazu (góra i lewa) 
+# 	łączysz je z odpowiadającymi punktami po przeciwnej stronie 
+# 	powstaje siatka linii przecinających obraz 
+# 	piksele na liniach → ustawiane na 0 (czarne) 
+# 📏 Parametr kluczowy
+# 	ρ_grids→ gęstość siatki 
+# 💡 Intuicja
+# 	nie jest to „inteligentny” atak 
+# 	raczej test odporności na zakłócenia strukturalne 
